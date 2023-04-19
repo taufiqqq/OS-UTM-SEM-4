437261b698a08d82cccabc2a6a2574f72fceff89
taufiqqq <muhdtaufiqbinjurimi@gmail.com>
2023-04-19 23:04:46 +0800

    Update Question 2.c

--- Lab Assessment 2/Question 2.c
+++ Lab Assessment 2/Question 2.c
@@ -2,42 +2,37 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main()
 {
     int id_parent = getpid();
+    int pid_child1, pid_child2, pid_child3, pid_child4;
+
     // Parent @ original process
     printf("I am the PARENT, my process identifier is %d, my clones are:\n", id_parent);
 
-    int pid_child1 = fork(); // Original create first child process (clone 1)
-
+    pid_child1 = fork(); // Original create first child process (clone 1)
     if (pid_child1 == 0)
-    { // Clone 1 will print
-        printf("I am the obsolete VERSION; the recent version is %d\n", getpid());
-
-        int pid_child2 = fork();
+    {
         // Clone 1 create it's own child
-        if (pid_child2 == 0)
-        {
-            // Clone 2 will print
-            printf("I am the new clone, process identifier of my parent is %d\n", getpid());
-        }
+        pid_child2 = fork();
+        if (pid_child2 > 0)
+            printf("I am the obsolete VERSION; the recent version is %d\n", pid_child2); // Clone 1 will print
     }
     else
     {
         // Original will have value pid_child1 > 1 thus
         // original will create second child process (clone 3)
-        int pid_child3 = fork();
+        pid_child3 = fork();
         if (pid_child3 == 0)
-        { // Clone 3 will print
-            printf("I am the SOPHISTICATED CLONE, process identifier of my parent is %d\n", getpid());
-
+        {
             // Clone 3 will make child, clone 4
-            int pid_child4 = fork();
+            pid_child4 = fork();
             if (pid_child4 == 0)
             {
-                // Clone 4 will print
-                printf("I am the most SOPHISTICATED CLONE, process identifier of my parent is %d\n", getpid());
+                // Clone 4 will print, and utilize getppid() to get parent id
+                printf("I am the most SOPHISTICATED CLONE, process identifier of my parent is %d\n", getppid());
             }
         }
 
@@ -45,6 +40,7 @@ int main()
         {
             printf("process clone1: %d\nprocess clone3: %d\n", pid_child1, pid_child3);
         }
+        wait(NULL);
     }
 
     return 0;
